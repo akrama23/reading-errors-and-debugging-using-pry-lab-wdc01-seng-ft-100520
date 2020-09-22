36f6f68925f7385162f516a2bff59a04a96161b7
akam alam <arundelfootball23@gmail.com>
2020-09-22 02:50:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
 
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+  star_date = (rand(100000) + 400000) / 10.0
 end
 
 
@@ -9,7 +9,7 @@ def state_log(star_date)
  "Captain's Log, star date #{star_date}."
 end
 
-def engage(date)
+def engage
   puts state_log(date)
    date = generate_star_date
 end
