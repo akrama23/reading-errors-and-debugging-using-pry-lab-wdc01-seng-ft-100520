0c8bc583321c31bc34efcffcd690d50d835fc3f6
akam alam <arundelfootball23@gmail.com>
2020-09-22 02:30:25 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] * 10 + string
-    10 * "s" 
+  if string[0] = "s"
+    return 10 * string[0] + string 
   else
     string
   end
