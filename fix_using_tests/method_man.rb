0daa6f4af24b6fa13479049739ea607e75f58104
akam alam <arundelfootball23@gmail.com>
2020-09-22 02:02:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,14 +1,14 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  player1 == "Rocky"
-  player2 == "Bullwinkle"
+  player1 = "Rocky"
+  player2 = "Bullwinkle"
   "Hello #{player1} & #{playar2}"
   
 end
 
-def play_game(player2)
-  player1== "Jay-Z"
-  player2== "Beyonce"
+def play_game(String)
+  player1= "Jay-Z"
+  player2= "Beyonce"
   "#{player1} is better than #{player2}"
 end
